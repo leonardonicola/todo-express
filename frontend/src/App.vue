d59b377eb54a2fbe56153b3fc8694dcd3dc44029
Leonardo Nicola <leonardonicolares@gmail.com>
2023-02-04 03:37:05 -0300

    feat: create Todo interface with all functionalities

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from 'vue'
+import { fetchData, addTodo } from './utils/FetchData'
+import type { ITask } from './utils/FetchData'
+
+const tasks = ref<ITask[]>([])
+const newTask = ref<string>('')
+
+const addNewTask = async (): Promise<void> => {
+  if (newTask.value !== '') {
+    tasks.value.push(await addTodo(newTask.value))
+    newTask.value = ''
+  }
+}
+
+onBeforeMount(async () => {
+  tasks.value = await fetchData()
+})
+</script>
+
+<template>
+  <main>
+    <input type="text" v-model.trim="newTask" />
+    <button @click="addNewTask()">ADD</button>
+    <div v-if="tasks.length > 0">
+      <p v-for="(task, index) in tasks" :key="index">
+        <input type="checkbox" name="taskCheck" :checked="task.done" />
+        {{ task.name }}
+      </p>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
