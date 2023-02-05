ed0656ad73632829da3933c11d62ba67e323a0a5
Leonardo Nicola <leonardonicolares@gmail.com>
2023-02-04 21:28:22 -0300

    refactor: use generics in POST method
    feat: use Axios to POST requests

--- frontend/src/utils/FetchData.ts
+++ frontend/src/utils/FetchData.ts
@@ -5,22 +5,15 @@ interface ITask {
   done: boolean
 }
 
-const fetchData = async (): Promise<ITask[]> => {
+const fetchData = (): Promise<ITask[]> => {
   return axios.get('http://localhost:3000/tasks').then((res) => res.data)
 }
 
-const addTodo = async (taskName: string): Promise<ITask> => {
-  const task: ITask = { name: taskName, done: false }
-  const res = await fetch(`http://localhost:3000/add`, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify(task),
-  })
-
-  if (!res.ok) throw new Error(res.statusText)
-  return res.json()
+const addTodo = <T extends ITask>(task: T): Promise<T> => {
+  return axios
+    .post('http://localhost:3000/add', task)
+    .then((res) => res.data)
+    .catch((error) => console.log(error))
 }
 
 export { fetchData, addTodo }
