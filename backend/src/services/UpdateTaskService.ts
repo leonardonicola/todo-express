f7344db9d32d37025dcf4c83e8adea3289f87041
Leonardo Nicola <leonardonicolares@gmail.com>
2023-02-06 02:57:33 -0300

    feat: add CRUD services

---
+++ backend/src/services/UpdateTaskService.ts
@@ -0,0 +1,28 @@
+import { PostgresDataSource } from '../config/ormconfig'
+import { Tasks } from '../entities/Tasks'
+
+type TaskUpdateRequest = {
+    id: string
+    name: string
+    done: boolean
+}
+
+export class UpdateTaskService {
+    async execute({id, name, done}: TaskUpdateRequest){
+        const repo = PostgresDataSource.getRepository(Tasks)
+
+        const task = await repo.findOneBy({id})
+
+        if(!task) {
+            return new Error("Task doesn't exists!")
+        }
+    
+        // If not passed argument, use own data
+        task.done = done !== undefined ? done : task.done
+        task.name = name ? name : task.name
+
+        await repo.save(task);
+
+        return task
+    }
+}
